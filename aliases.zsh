35c796d0ca41a0854b1b721634a467bb13d963a0
jmattaa <mattajonathan1@gmail.com>
2024-12-28 17:31:56 +0200

    remove tn alias and now it's a script in the scripts repo

--- aliases.zsh
+++ aliases.zsh
@@ -1,14 +1,4 @@
 alias t="tmux"
-tn() {
-    if [ -z "$1" ]; then
-        session_name="$(basename "$(pwd)")"
-    else
-        session_name="$1-$(basename "$(pwd)")"
-    fi
-    tmux new -s "$session_name"
-} 
-
-alias fcd="source ~/.fcd/fcd"
 
 # alias thefuck
 eval $(thefuck --alias)
