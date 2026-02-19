466b84c6cba17aeb047d4fef733a8bcf11274ad7
jmattaa <mattajonathan1@gmail.com>
2026-02-19 21:04:18 +0100

    update the arrow to work now in prompt

--- prompt.zsh
+++ prompt.zsh
@@ -2,8 +2,11 @@ function git_status_symbols() {
     local gstatus=$(git status --porcelain=2 --branch 2> /dev/null)
     local symbols=""
 
-    [[ $gstatus =~ "# branch.ab \+([0-9]+)" ]] && symbols+="⇡"
-    [[ $gstatus =~ "# branch.ab \-([0-9]+)" ]] && symbols+="⇣"
+    local ahead=$(echo "$gstatus" | grep '^# branch.ab ' | sed -E 's/^# branch.ab \+([0-9]+) -([0-9]+)/\1/')
+    local behind=$(echo "$gstatus" | grep '^# branch.ab ' | sed -E 's/^# branch.ab \+([0-9]+) -([0-9]+)/\2/')
+
+    [[ $ahead -gt 0 ]] && symbols+="⇡"
+    [[ $behind -gt 0 ]] && symbols+="⇣"
 
     echo "$gstatus" | grep -q '^1 .*U' && symbols+="="
     echo "$gstatus" | grep -q '^? ' && symbols+="?"
